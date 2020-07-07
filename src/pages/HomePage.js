97b03ff253989b93c40ccc878d8d55e861b9ca33
mahmoudfathy557 <mahmoudfathy557@gmail.com>
2020-07-07 03:50:52 +0200

    app-is-finished

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,18 @@
 import React from "react";
-
+import Hero from "../components/Hero";
+import { Link } from "react-router-dom";
+import Services from "../components/HomePage/Services";
+import Featured from "../components/HomePage/Featured";
 export default function HomePage() {
   return (
     <>
-      <h1>Hello From Home Page</h1>
+      <Hero title="awesome gadgets" max="true">
+        <Link to="/products" className="main-link" style={{ margin: "2rem" }}>
+          our products
+        </Link>
+      </Hero>
+      <Services />
+      <Featured />
     </>
   );
 }
